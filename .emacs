f57b4cad150fec712fe7421a69cd7c34b9a192c7
MaskRay <emacsray@gmail.com>
2010-11-13 15:50:26 +0800

    added keywize

--- .emacs
+++ .emacs
@@ -1,4 +1,3 @@
 (progn (cd "~/.emacs.d") (normal-top-level-add-subdirs-to-load-path) (cd "~"))
 (add-to-list 'load-path "~/.emacs.d")
-(server-start)
-(mapc 'require '(fonts color-theme-irblack utils modes settings plugins keybindings org-mode-settings erc-settings))
+(mapc 'require '(color-theme-irblack utils modes settings plugins keybindings org-mode-settings erc-settings))
--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -1,15 +1,25 @@
-(recentf-mode 1)
-(defalias 'yes-or-no-p 'y-or-n-p)
-(global-linum-mode 1)
-(column-number-mode 1)
-(blink-cursor-mode -1)
-(mouse-avoidance-mode 'animate)
-(global-font-lock-mode t)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(auto-image-file-mode 1)
+(defun frame-setting ()
+  (blink-cursor-mode -1)
+  (mouse-avoidance-mode 'animate)
+  (global-linum-mode 1)
+  (column-number-mode 1)
+  (global-font-lock-mode t)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (show-paren-mode 1)
+  (scroll-bar-mode -1)
+  (auto-image-file-mode 1)
+  (require 'fonts)
+  (window-system-font-setting)
+  )
+(if (and (fboundp 'daemonp) (daemonp))
+    (add-hook 'after-make-frame-functions
+	      (lambda (frame)
+		(with-selected-frame frame
+		  (frame-setting))))
+  (frame-setting)) 
 
+(defalias 'yes-or-no-p 'y-or-n-p)
 (setq user-full-name "MaskRay"
       user-mail-address "emacsray@gmail.com"
       next-screen-context-lines 5
@@ -30,10 +40,10 @@
 	      truncate-partial-width-windows 15)
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(ac-selection-face ((t (:background "blue" :foreground "white")))))
 
 
