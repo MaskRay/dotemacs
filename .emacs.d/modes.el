d9ded81c69bb289f4ef1f45ce6c09b4bad678b3d
MaskRay <emacsray@gmail.com>
2010-11-21 11:20:09 +0800

    added smart-compile

--- .emacs.d/modes.el
+++ .emacs.d/modes.el
@@ -49,6 +49,43 @@
 
 
 ;; cc
+(defun smart-compile()
+  "比较智能的C/C++编译命令
+如果当前目录有makefile则用make -k编译，否则，如果是
+处于c-mode，就用gcc -Wall编译，如果是c++-mode就用
+g++ -Wall编译"
+  (interactive)
+  ;; 查找 Makefile
+  (let ((candidate-make-file-name '("makefile" "Makefile" "GNUmakefile"))
+        (command nil))
+    (if (not (null
+              (find t candidate-make-file-name :key
+                    '(lambda (f) (file-readable-p f)))))
+        (setq command "make -k ")
+        ;; 没有找到 Makefile ，查看当前 mode 是否是已知的可编译的模式
+        (if (null (buffer-file-name (current-buffer)))
+            (message "Buffer not attached to a file, won't compile!")
+            (if (eq major-mode 'c-mode)
+                (setq command
+                      (concat "gcc -Wall -o "
+                              (file-name-sans-extension
+                               (file-name-nondirectory buffer-file-name))
+                              " "
+                              (file-name-nondirectory buffer-file-name)
+                              " -g -lm "))
+              (if (eq major-mode 'c++-mode)
+                  (setq command
+                        (concat "g++ -Wall -o "
+                                (file-name-sans-extension
+                                 (file-name-nondirectory buffer-file-name))
+                                " "
+                                (file-name-nondirectory buffer-file-name)
+                                " -g -lm "))
+                (message "Unknow mode, won't compile!")))))
+    (if (not (null command))
+        (let ((command (read-from-minibuffer "Compile command: " command)))
+          (compile command)))))
+
 (defun my-c-mode-common-hook ()
   (c-set-style "k&r")
   (setq tab-width 4 indent-tabs-mode nil c-basic-offset 4)
@@ -59,7 +96,7 @@
   (add-to-list 'c-cleanup-list 'brace-catch-brace)
   (add-to-list 'c-cleanup-list 'defun-close-semi)
   (add-to-list 'c-cleanup-list 'one-liner-defun)
-  (define-key c-mode-base-map [f5] 'compile)
+  (define-key c-mode-base-map [f5] 'smart-compile)
   (define-key c-mode-base-map "\C-m" 'c-context-line-break)
   (define-key c-mode-base-map (kbd "M-/") 'semantic-ia-complete-symbol-menu)
   (define-key c-mode-base-map [f12] 'semantic-ia-fast-jump)
